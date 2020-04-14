c74f6e84f4e00106a4dbd4899cc47b0eaa8ef536
rlee38 <47578853+rlee38@users.noreply.github.com>
2020-04-14 17:17:41 -0500

    test

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+var a = 0
+
 exports.httpReq = functions.https.onRequest(app);
 
 app.use(express.urlencoded({ extended: false }))
