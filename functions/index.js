92fda3706627d6688340b9e53a8fbfe2cee378ab
Shi Zhe Ting <sting7@uco.edu>
2020-04-14 17:54:07 -0500

    test

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-var a = 0
-
 exports.httpReq = functions.https.onRequest(app);
 
 app.use(express.urlencoded({ extended: false }))
